916e8766e55b413db1b6dd7b30836fd1cfb8dc47
ReX342 <occ.rex@gmail.com>
2021-12-01 21:13:30 +0100

    3models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction_Listings():
+    pass
+
+class Bids():
+    pass
+
+class comments_AL():
+    #foreignkey: on delete CASCADE
+    # Auction_Listings is parent
+    pass
