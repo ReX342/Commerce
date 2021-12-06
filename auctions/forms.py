19e6ecd5c9c2bfe18ca9fbbb988d1b7fd89eeb8a
ReX342 <occ.rex@gmail.com>
2021-12-06 23:16:04 +0100

    cantcleanupmodelsuntilnewdb

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,17 +1,11 @@
 from django.forms import ModelForm
-from .models import User, Auction_Listings, WatchList, Bids
+from .models import User, Auction_Listings, Bids
 from django import forms
 
 class Auction_ListingsForm(ModelForm):
     class Meta:
         model = Auction_Listings
         fields = ['title', 'description', 'category', 'starting_bid', 'image_file', 'image_url', 'quantity' ]
-
-class WatchListForm(ModelForm):
-    watch_form = forms.BooleanField(initial=True)
-    class Meta:
-        model = WatchList
-        fields = ['watching', 'watch_form']
 
 class BidsForm(ModelForm):
     class Meta:
