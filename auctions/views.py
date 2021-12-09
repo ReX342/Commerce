c1dd968974a623063826891c30c06ab78a5769c2
ReX342 <occ.rex@gmail.com>
2021-12-09 20:46:49 +0100

    want to clean up models and delete migrations if needed

--- auctions/views.py
+++ auctions/views.py
@@ -144,79 +144,7 @@ def unwatch_this(request,id):
     logged_in_user.wishlisted.remove(Auction_Listings.objects.get(id=listing_id))         
     return HttpResponseRedirect(reverse("watchlist"))
 
-@login_required
-def bids(request, id):
-    # get Auction_Listings.current_bid
-    # in a nice form where user(id=id) can update
-    logged_in_user = User.objects.get(id=request.user.id)
-    AL = Auction_ListingsForm   
-    bids = BidsForm()
-    st_bid = Bids
-    if request.method == 'POST':
-        bids = BidsForm(request.POST)
-        current_bid = AL.current_bid
-        starting_bid = AL.starting_bid
-
-        if current_bid > starting_bid:
-            if bids.is_valid():             
-                bids.save()            
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            bids = BidsForm()
-       
-    return render(request, "auctions/watch.html", {
-        "bids" : bids
-    })
-
-#I give up: listing.html url bids as alternative
-@login_required
-def bid(request, id):
-    logged_in_user = User.objects.get(id=request.user.id)
-    AL = Auction_ListingsForm   
-    bids = BidsForm()
-    st_bid = Bids
-    
-    bids = BidsForm(request.POST)
-    current_bid = BidsForm.current_bid
-    current = Bids.objects.get(current_bid=request.current_bid)
-    if request.method == 'POST':
-        bids = BidsForm(request.POST)
-        current_bid = st_bid.current_bid
-        starting_bid = st_bid.starting_bid
-        if current_bid > starting_bid:
-            if bids.is_valid():             
-                bids.save()            
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            bids = BidsForm()
-       
-    return render(request, "auctions/watch.html", {
-        "bids" : bids
-    })
-
-def bid_this(request, id):
-    logged_in_user = User.objects.get(id=request.user.id)
-    listing_id = id
-    # logged_in_user.bid.update(Auction_Listings.objects.get(id=listing_id))         
-    form = BidsForm(request.POST)
-    if form.is_valid():
-        current_bid = request.POST["current_bid"]             
-        #current_bid  = form.cleaned_data["current_bid"]
-        f = Bids(current_bid="current_bid")
-        f.save()
-    return HttpResponseRedirect(reverse("index"))
-
-def compare_bids(request):
-    if request == "POST":
-        bids = request.POST(Bids)
-        if bids.current_bid > bids.starting_bid:
-            current_bid = bids.current_bid
-            current_bid.save()
-        else:
-            # Message user (in a more user friendly way)
-            return HttpResponse("Your opening bid must be higher")
-        return HttpResponseRedirect(reverse("index"))
-    
+  
 @login_required
 def placebid(request, id):
     auction = Auction_Listings.objects.get(id=id)
@@ -234,28 +162,3 @@ def placebid(request, id):
     return HttpResponseRedirect(reverse("index"))
 
  
-
-
-# form action="/place_bid"
-
-# <input type="hidden" name="listing_id" value="{{ listing.id }}">
-# <input type="text" name="amount">
-
-# --- in views
-
-# place_bid function
-
-# Nodig
-# - Huidige gebruiker (te vinden in request.user dacht ik?)
-# - Huidige bod amount (te vinden in request.POST['amount']
-# - Huidige listing id, om de effectieve listing te kunnen terugvinden. (te vinden in request.POST['amount']
-
-# Stappen
-# 1) Haal listing op aan de hand van id
-# 2) Haal bids op van de listing (sorteer op amount)
-# 3) ....
-
-# Einde van de fucntie moet een redirect zijn naar de listing.
-# Er zijn twee uitkomsten
-# - bid gelukt yay
-# - bid niet gelukt, geef de gebruiker een melding waarom. (not authenticated, amount too low ...)
